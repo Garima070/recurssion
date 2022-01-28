1ed6e832d2dca72846885f78e7beb334b8b37aae
RICKY <grajpootga@gmail.com>
2022-01-29 01:06:16 +0530

    initial commit

--- src/indexarray.java
+++ src/indexarray.java
@@ -14,7 +14,8 @@ public class indexarray {
         Scanner s=new Scanner(System.in);
         int[] arr=new int[]{2,3,4,5,6,4};
         int target=4;
-        ArrayList<Integer> ans=allindex(arr,0,target,new ArrayList<>());
+        ArrayList<Integer> list=new ArrayList<>();
+        ArrayList<Integer> ans=allindex(arr,0,target,list);
         System.out.println(ans);
 
     }
